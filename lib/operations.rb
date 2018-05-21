eaa24dc00686cb71063f572f3c211b369262f588
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-21 21:59:27 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,17 @@
 def unsafe?(speed)
-if 
-  return
-elsif
-  return
-else
-  return
+if speed > 60
+  return "unsafe"
+elsif speed < 40
+  return "unsafe"
+else 40 <= speed <= 60
+  return "safe"
 end 
 end
 
 
 
 def not_safe?(speed)
-	speed <40 || speed > 60? "not_safe" : "safe" 
+	speed < 40 || speed > 60? "not_safe" : "safe" 
 end
 	
 
